3328a1d3edc48e0e5a29eba7c4271d8b47b37a23
Lukasz Jedral <lukasjedral@gmail.com>
2021-10-04 22:18:18 +0200

    def check - urls parameter is no longer optional

--- uptimer/uptimer.py
+++ uptimer/uptimer.py
@@ -4,6 +4,12 @@ from time import sleep
 
 
 def check_url(url):
+    """Send HEAD request to the url and return the HTTP status code
+    :param url: URL to check
+    :type url: str
+    :return: HTTP status code
+    :rtype: int
+    """
     try:
         response = requests.head(url)
     except requests.exceptions.ConnectionError:
@@ -13,6 +19,12 @@ def check_url(url):
 
 
 def colorize_status(url, status):
+    """Print the URL and status in color to the terminal
+    :param url: URL to print
+    :type url: str
+    :param status: status used to determine the color of the output
+    :type status: str
+    """
     # fmt: off
     colors = {
         2: "green",
@@ -25,9 +37,16 @@ def colorize_status(url, status):
 
 
 @click.command()
-@click.argument("urls", nargs=-1)
+@click.argument("urls", nargs=-1, required=true)
 @click.option("--daemon", "-d", default=False, is_flag=True)
 def check(urls, daemon):
+    """Check urls and print their HTTP statuses (with colors)
+    :param urls: URL (or a tuple with multiple URLs) to check
+    :type urls: str or tuple(str)
+    :param daemon: If set to True, after checking all URLs,
+                   sleep for 5 seconds and check them again
+    :type daemon: bool
+    """
     while True:
         for url in urls:
             status_code = check_url(url)
